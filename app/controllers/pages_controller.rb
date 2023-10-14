2190d6ac1466c8edf7ef7cc48cc28e24bef1f117
Sergio Dominguez <sergio.castle@gmail.com>
2023-10-13 23:20:13 -0300

    Feat: creacion controlador pages y vistas estaticas

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,13 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def service
+  end
+
+  def workinghour
+  end
+
+  def contact
+  end
+end
---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get home" do
+    get pages_home_url
+    assert_response :success
+  end
+
+  test "should get service" do
+    get pages_service_url
+    assert_response :success
+  end
+
+  test "should get workinghour" do
+    get pages_workinghour_url
+    assert_response :success
+  end
+
+  test "should get contact" do
+    get pages_contact_url
+    assert_response :success
+  end
+end
